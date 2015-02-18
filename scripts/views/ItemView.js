ec4c2de411b97419ecdb7745fa31622b7a6f3da9
Charles Lueker <charleslueker@yahoo.com>
2015-02-18 15:47:06 -0600

    working strikethrough

--- scripts/views/ItemView.js
+++ scripts/views/ItemView.js
@@ -7,14 +7,21 @@ var ItemView = Backbone.View.extend({
 			);
 		var itemTemplate = _.template($('#my-list').html());
 		this.$item = $(itemTemplate(this.model.attributes));
+		console.log(this.$item);
 		this.$checkBox = $(this.$item.find('.check-box'));
-
+		this.$description = $(this.$item.find('.item'));
 	
 		this.$checkBox.on('click', this.onStrikeThrough);
 	},
 
 	onStrikeThrough: function(){
-		$(this.$description).css("text-decoration", "line-through");
-	
+		console.log('onStrikeThrough');
+		console.log(this.$checkBox.is(':checked'));
+		
+		if(this.$checkBox.is(':checked')===true){
+			$(this.$description).css("text-decoration", "line-through");
+		}else{
+			$(this.$description).css("text-decoration", "none");
+		}
 	}
 });
